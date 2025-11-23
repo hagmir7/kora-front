e7d0eefb45430652adaab3e07f6f23083e32fad6
Agmir Hassane <hagmir6@gmail.com>
2025-11-23 18:48:41 +0100

    edit next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   /* config options here */
   images: {
     domains: ['prod-media.beinsports.com', 'jdwel.com', 'assets-us-01.kc-usercontent.com', 'www.beinsports.com'],
+    unoptimized: true,
   },
 };
 
